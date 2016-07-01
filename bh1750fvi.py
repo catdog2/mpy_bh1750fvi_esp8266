247ea4110bf607312c3410d5f84b474783443be0
Peter Dahlberg <catdog2@tuxzone.org>
2016-07-01 18:11:30 +0200

    we need to wait max time

--- bh1750fvi.py
+++ bh1750fvi.py
@@ -4,8 +4,8 @@ OP_SINGLE_HRES1 = 0x20
 OP_SINGLE_HRES2 = 0x21
 OP_SINGLE_LRES = 0x23
 
-DELAY_HMODE = 120000  # 120ms in H-mode
-DELAY_LMODE = 16000  # 16ms in L-mode
+DELAY_HMODE = 180  # 180ms in H-mode
+DELAY_LMODE = 24  # 24ms in L-mode
 
 
 def sample(i2c, mode=OP_SINGLE_HRES1, i2c_addr=0x23):
@@ -17,7 +17,7 @@ def sample(i2c, mode=OP_SINGLE_HRES1, i2c_addr=0x23):
     i2c.writeto(i2c_addr, b"\x01")  # power up
     i2c.writeto(i2c_addr, bytes([mode]))  # set measurement mode
 
-    time.sleep_us(DELAY_LMODE if mode == OP_SINGLE_LRES else DELAY_HMODE)
+    time.sleep_ms(DELAY_LMODE if mode == OP_SINGLE_LRES else DELAY_HMODE)
 
     raw = i2c.readfrom(i2c_addr, 2)
     i2c.writeto(i2c_addr, b"\x00")  # power down again
